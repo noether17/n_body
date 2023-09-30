5b9bbc8111f524d709d40974405e23af5e3d0ac7
Jonathan White <jawh@umich.edu>
2023-09-30 17:37:51 -0600

    Created struct to separately contain positions and velocities.

--- src/n_body_simulator/n_body_simulator.cpp
+++ src/n_body_simulator/n_body_simulator.cpp
@@ -6,25 +6,30 @@
 #include <vector>
 
 constexpr auto N = 1 << 10; // number of particles
-constexpr auto dof = 6; // degrees of freedom per particle
+constexpr auto dim = 3; // number of spatial dimensions
 constexpr auto L = 10.0; // box length
 
-void output_states(double t, const std::vector<double>& states, const std::string& fn);
+struct State
+{
+    std::vector<double> pos{};
+    std::vector<double> vel{};
+};
+
+void output_states(double t, const State& state, const std::string& fn);
 
 int main()
 {
     // initialize states
-    auto state_vector = std::vector<double>(dof*N);
+    auto state = State{};
+    state.pos = std::vector<double>(dim*N);
+    state.vel = std::vector<double>(dim*N);
     auto rd = std::random_device{};
     auto gen = std::mt19937{rd()};
     auto dist = std::uniform_real_distribution<double>(L);
-    for (auto i = 0; i != state_vector.size() / 2; ++i)
-    {
-        state_vector[i] = dist(gen);
-    }
+    for (auto& pos : state.pos) { pos = dist(gen); }
 
     // print states
-    output_states(0.0, state_vector, "output.txt");
+    output_states(0.0, state, "output.txt");
 
     // rotate around z-axis
     constexpr auto theta = 2.0 * 3.14159265358979323 / 100.0;
@@ -35,30 +40,29 @@ int main()
         auto t = t_idx * 0.1;
         for (auto i = 0; i < N; ++i)
         {
-            auto x = state_vector[3*i];
-            auto y = state_vector[3*i + 1];
-            state_vector[3*i] = cos_theta*x - sin_theta*y;
-            state_vector[3*i + 1] = sin_theta*x + cos_theta*y;
+            auto x = state.pos[3*i];
+            auto y = state.pos[3*i + 1];
+            state.pos[3*i] = cos_theta*x - sin_theta*y;
+            state.pos[3*i + 1] = sin_theta*x + cos_theta*y;
         }
-        output_states(t, state_vector, "output.txt");
+        output_states(t, state, "output.txt");
     }
 
     return 0;
 }
 
-void output_states(double t, const std::vector<double>& states, const std::string& fn)
+void output_states(double t, const State& state, const std::string& fn)
 {
     auto output_file = std::ofstream{fn, std::ios_base::app};
     output_file << std::setprecision(16);
-    auto vel_offset = states.size() / 2;
-    for (auto i = 0; i != states.size() / dof; ++i)
+    for (auto i = 0; i != state.pos.size() / dim; ++i)
     {
         output_file << t << ','
-                    << states[i*3] << ','
-                    << states[i*3 + 1] << ','
-                    << states[i*3 + 2] << ','
-                    << states[i*3 + vel_offset] << ','
-                    << states[i*3 + vel_offset + 1] << ','
-                    << states[i*3 + vel_offset + 2] << '\n';
+                    << state.pos[i*3] << ','
+                    << state.pos[i*3 + 1] << ','
+                    << state.pos[i*3 + 2] << ','
+                    << state.vel[i*3] << ','
+                    << state.vel[i*3 + 1] << ','
+                    << state.vel[i*3 + 2] << '\n';
     }
 }
