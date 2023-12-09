3570146e6688e4485c261c60568c8854349d8599
Jonathan White <jawh@umich.edu>
2023-12-09 14:31:17 -0700

    Added file output to threaded n body simulator.

--- src/n_body_simulator/threaded_n_body_simulator.cpp
+++ src/n_body_simulator/threaded_n_body_simulator.cpp
@@ -5,7 +5,7 @@
 #include "physics.hpp"
 #include "Vector3d.hpp"
 
-constexpr auto N = 1 << 12; // number of particles
+constexpr auto N = 1 << 3; // number of particles
 
 int main()
 {
@@ -15,7 +15,7 @@ int main()
     state.vel = std::vector<Vector3d>(N);
     auto seed = 25UL;
     auto gen = std::mt19937{seed};
-    auto dist = std::uniform_real_distribution<double>(L);
+    auto dist = std::uniform_real_distribution<double>(0.0, L);
     for (auto& pos : state.pos)
     {
         pos = Vector3d{dist(gen), dist(gen), dist(gen)};
@@ -23,7 +23,8 @@ int main()
 
     // simulate
     auto max_time = characteristic_time(N, L);
-    threaded_euler(state, 1e-3*max_time, max_time, 4);
+    auto dt = 1e-1*max_time;
+    threaded_euler(state, dt, max_time, 4);
 
     return 0;
 }
--- src/ode/ode.hpp
+++ src/ode/ode.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <barrier>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
 #include <span>
+#include <sstream>
 #include <thread>
 #include <vector>
 
@@ -29,32 +35,85 @@ void threaded_euler_step(ThreadData& data, AccFunc acc_func,
     for (auto i = 0; i < thread_size; ++i) { data.vel[i] += data.acc[i]*data.dt; }
 }
 
+struct OutputEntry
+{
+    double t{0.0};
+    std::size_t index{0};
+    Vector3d pos{};
+    Vector3d vel{};
+};
+
 template <typename AccFunc, typename BarrierType>
 void threaded_euler_loop(ThreadData data, double max_time, AccFunc acc_func,
-    BarrierType& barrier)
+    BarrierType& barrier, std::mutex& mx, std::vector<OutputEntry>& out_states)
 {
     auto t = 0.0;
+    auto local_out_states = out_states;
     while (t < max_time)
     {
         threaded_euler_step(data, acc_func, barrier);
         t += data.dt;
+
+        for (std::size_t i = 0; i < data.vel.size(); ++i)
+        {
+            auto pos_index = i + data.offset;
+            local_out_states.emplace_back(t, pos_index, data.pos[pos_index], data.vel[i]);
+        }
     }
+
+    std::scoped_lock lock{mx};
+    out_states = std::move(local_out_states);
+}
+
+auto generate_filename(int system_size, int n_threads) -> std::string
+{
+    auto now = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+    auto filename = std::stringstream{};
+    filename << "output_" << system_size << '_' << n_threads << '_' << now_c << ".csv";
+    return filename.str();
 }
 
 void threaded_euler(State& state, double dt, double max_time, int n_threads)
 {
     auto pos = std::span<Vector3d>{state.pos};
-    auto thread_size = pos.size() / n_threads;
+    auto system_size = pos.size();
+    auto thread_size = system_size / n_threads;
     auto acc_portions = std::vector(n_threads, std::vector<Vector3d>(thread_size));
     auto barrier = std::barrier{n_threads};
     auto threads = std::vector<std::jthread>{};
+    auto thread_output_vecs = std::vector(n_threads, std::vector<OutputEntry>{});
+    std::mutex mx;
     for (auto i = 0; i < n_threads; ++i)
     {
         auto offset = i*thread_size;
         auto vel_portion = std::span<Vector3d>{state.vel}.subspan(offset, thread_size);
         auto acc_portion = std::span<Vector3d>{acc_portions[i]};
         auto data = ThreadData{pos, vel_portion, acc_portion, dt, offset};
-        auto thread_function = [&](){ threaded_euler_loop(data, max_time, threaded_gravity, barrier); };
-        threads.emplace_back(thread_function);
+        auto& thread_output = thread_output_vecs[i];
+        auto thread_function = [&barrier, &mx, &thread_output]
+            (ThreadData data, double max_time, auto acc_func)
+        {
+            threaded_euler_loop(data, max_time, threaded_gravity, barrier, mx, thread_output);
+        };
+        threads.emplace_back(thread_function, data, max_time, threaded_gravity);
+    }
+    for (auto& thread : threads) { thread.join(); }
+
+    auto filename = generate_filename(system_size, n_threads);
+    auto out = std::ofstream{filename};
+    for (auto& thread_states : thread_output_vecs)
+    {
+        for (auto& state : thread_states)
+        {
+            out << state.t << ','
+                << state.index << ','
+                << state.pos.x << ','
+                << state.pos.y << ','
+                << state.pos.z << ','
+                << state.vel.x << ','
+                << state.vel.y << ','
+                << state.vel.z << '\n';
+        }
     }
 }
