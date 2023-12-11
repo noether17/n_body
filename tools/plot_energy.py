8bc179724faed31449d15c6226169b462b4c7736
Jonathan White <jawh@umich.edu>
2023-12-10 17:55:08 -0700

    Parallelized calculation of energy and added labels to plot.

--- tools/plot_energy.py
+++ tools/plot_energy.py
@@ -1,6 +1,10 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+from numba import jit, prange, set_num_threads
+import os
+
+set_num_threads(4)
 
 G = 6.67e-11 # gravitational constant
 
@@ -9,6 +13,8 @@ def main():
     parser.add_argument('filename')
     args = parser.parse_args()
     filename = args.filename
+
+    system_size = int(os.path.basename(filename).split('_')[1])
 
     raw_states = np.loadtxt(filename, delimiter=',', dtype=float)
     times = np.unique(raw_states[:, 0])
@@ -20,11 +26,22 @@ def main():
         frames.append(current_frame)
     frames = np.array(frames)
 
-    energies = np.array([compute_energy(frame) for frame in frames])
+    energies = compute_energy_parallel(frames)
 
     plt.plot(times, energies)
-    plt.show()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Energy (J)")
+    plt.title(f"System Energy over Time ({system_size} particles)")
+    plt.savefig(f"energy_{system_size}.png")
 
+@jit(nopython=True, parallel=True)
+def compute_energy_parallel(frames):
+    energies = np.empty(frames.shape[0])
+    for i in prange(frames.shape[0]):
+        energies[i] = compute_energy(frames[i])
+    return energies
+
+@jit(nopython=True)
 def compute_energy(frame):
     pos = frame[:, 0:3]
     vel = frame[:, 3:6]
@@ -33,6 +50,7 @@ def compute_energy(frame):
     kinetic_energy = compute_kinetic_energy(vel)
     return potential_energy + kinetic_energy
 
+@jit(nopython=True)
 def compute_potential_energy(pos):
     n_particles = pos.shape[0]
     potential_energy = 0.0
@@ -42,6 +60,7 @@ def compute_potential_energy(pos):
             potential_energy -= 1.0 / r
     return G*potential_energy
 
+@jit(nopython=True)
 def compute_kinetic_energy(vel):
     return 0.5 * np.sum(vel*vel)
 
